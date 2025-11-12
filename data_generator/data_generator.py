a452d15b3674c2ce39d5d01cde50bca5de861098
Winnie-Udel <winnie@udel.edu>
2025-11-12 18:40:51 -0500

    Add a function that'll compute main lambda based on history of main lambdas of previous time points

--- data_generator/data_generator.py
+++ data_generator/data_generator.py
@@ -1,4 +1,5 @@
 import json
+import csv
 import numpy as np
 
 """
@@ -50,3 +51,43 @@ def assign_service_rates(queue_network: dict):
     return queue_network
 
 print(assign_service_rates(queue_network))
+
+# No noise is added as of now. BUT SHOULD BE IMPLEMENTED (prehaps in another function?). 
+# For ease of testing, there's no noise added. 
+def compute_curr_lambda(main_lambdas, k, alpha, C) -> float:
+    """
+    Compute λ_main for the current timepoint based on previous timepoints. 
+    Let t be the timepoint. So t+1 is the current time point, and the lambda 
+    is computed as follow: 
+
+    λ_{t+1} = αλ_t + α²λ_{t-1} + ... + αᵏλ_{t+1−k} + C
+    
+    Args: 
+        main_lambdas (list[int]): List keeping track of all the main_lambda values from 
+        all the time point. 
+        k (float): How long is the dependency of λ is. 
+        alpha (float): How much λ is dependent on the previous time point.
+        C (float): Constant 
+
+    Returns:
+        main_lambda (float): The lambda value of the current time point. 
+    """
+    main_lambda = 0.0
+    n = len(main_lambdas)
+    
+    # Loop over the last k previous lambdas
+    for i in range(1, k+1):
+        if n - i < 0:
+            break  # Not enough history yet, so loop over existing ones
+        main_lambda += (alpha ** i) * main_lambdas[-i]
+
+    main_lambda += C
+    return main_lambda
+
+# These 2 should equal to each other 
+print(compute_curr_lambda([0.2,0.3,0.4], 2, 0.5, 0.1))
+print((0.5*0.4)+((0.5**2)*0.3)+0.1) # k = 2, alpha = 0.5, C = 0.1
+
+# These 2 should equal to each other 
+print(compute_curr_lambda([0.3], 2, 0.5, 0.1)) # When there is not enough history 
+print(0.5*0.3+0.1) # k = 2, alpha = 0.5, C = 0.1
