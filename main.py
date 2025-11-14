8178901f0f9fc6ba688a4d80f9bd585bda3c683a
TristanDend <tdend413@gmail.com>
2025-11-14 07:03:33 -0500

    Added a user input testing file specifically for the presentation.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from program_files import config,data_conversion #,data_generator
+from program_files import config,data_conversion,user_input #,data_generator
 from pathlib import Path
-
-
 
 def print_new_section(title:str):
     print(f"\n\n"
@@ -34,9 +32,14 @@ def test_validate_schema():
     print_new_section("Validate Schema")
 
 
-
     return None
 
+def test_user_input():
+    print_new_section("System Description")
+    user_config_settings = user_input.ask_user()
+    if user_config_settings is not None:
+        config.set_user_config("user_config.ini", user_config_settings)
+    return None
 
 def main():
     print_new_section("IBM Stress Testing")
@@ -46,6 +49,7 @@ def main():
                     "1: config.py\n"
                     "2: data_conversion.py\n"
                     "3: data-generator.py\n"
+                    "4: user_input.py\n"
                     "0: exit program\n")
 
         if inp == "1":
@@ -54,6 +58,8 @@ def main():
             print("2")
         elif inp == "3":
             print("3")
+        elif inp == "4":
+            test_user_input()
         elif inp == "0":
             print("exiting program")
             break
